a67b3b9533ffe7e1f880327227f88c5df01dd3f9
Lucas Fleischer <lfleischer@itba.edu.ar>
2022-11-19 16:08:59 -0300

    Protocol parser files (empty)

---
+++ src/server/protocol.c
@@ -0,0 +1,9 @@
+/**
+ * protocol.c -- archivo encargado del parseo del protocolo creado
+ */
+#include <string.h> //memset
+#include <arpa/inet.h> //ntohs
+#include <stdio.h>
+#include <stdio.h> // printf
+
+#include "protocol.h"
