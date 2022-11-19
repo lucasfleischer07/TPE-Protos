eeeab42434bd6833f6ee064ea43aa1e74954514a
Lucas Fleischer <lfleischer@itba.edu.ar>
2022-11-19 16:09:33 -0300

    Adding .h of protocol.c

---
+++ src/include/protocol.h
@@ -0,0 +1,11 @@
+#ifndef MONITOR_H
+#define MONITOR_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "buffer.h"
+
+/** Archivo encargado del manejo del protocolo, parseos y funciones*/
+
+
+#endif
