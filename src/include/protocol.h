1ace3bc663cb8274d41565134500922ba0c4e6ce
Ian <ianszejer@gmail.com>
2022-11-20 00:04:23 -0300

    Functions for all the parsing stages done

--- src/include/protocol.h
+++ src/include/protocol.h
@@ -1,5 +1,5 @@
-#ifndef MONITOR_H
-#define MONITOR_H
+#ifndef protocol_H
+#define protocol_H
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -8,6 +8,9 @@
 //Tamaño de username y password segun la RFC 1929, deben tener un tamaño entre 1 y 255 bytes
 #define USERNAME_SIZE 256
 #define PASSWORD_SIZE 256
+
+#define IS_ALNUM(x) (x>='a' && (x) <= 'z') || (x>='A' && x <= 'Z') || (x>='0' && x <= '9')
+
 
 /** Archivo encargado del manejo del protocolo, parseos y funciones*/
 /**
@@ -38,37 +41,37 @@
 */  
 
 #define TOKEN_SIZE 16
+#define DLEN_SIZE 0x2
 
-
-enum monitor_state {            
-    monitor_version,
-    monitor_token,
-    monitor_method,
-    monitor_dlen,
-    monitor_data,
+enum protocol_state {            
+    protocol_version,
+    protocol_token,
+    protocol_method,
+    protocol_dlen,
+    protocol_data,
 
     // apartir de aca estan done
-    monitor_done,
+    protocol_done,
 
     // y apartir de aca son considerado con error 
-    monitor_error,
-    monitor_error_unsupported_version,
-    monitor_error_invalid_token,
-    monitor_error_unsupported_method,
-    monitor_error_invalid_data,
+    protocol_error,
+    protocol_error_unsupported_version,
+    protocol_error_invalid_token,
+    protocol_error_unsupported_method,
+    protocol_error_invalid_data,
 };
 
-enum monitor_response_status {
-    monitor_status_succeeded                            = 0x00,
-    monitor_status_invalid_version                      = 0x01,
-    monitor_status_invalid_method                       = 0x02,
-    monitor_status_invalid_data                         = 0x04,
-    monitor_status_error_auth                           = 0x05,
-    monitor_status_server_error                         = 0x06,
+enum protocol_response_status {
+    protocol_status_succeeded                            = 0x00,
+    protocol_status_invalid_version                      = 0x01,
+    protocol_status_invalid_method                       = 0x02,
+    protocol_status_invalid_data                         = 0x04,
+    protocol_status_error_auth                           = 0x05,
+    protocol_status_server_error                         = 0x06,
 
 };
 
-enum monitor_method {
+enum protocol_method {
     get_historic                = 0x00,
     get_concurrent              = 0x01,
     get_transfered              = 0x02,
@@ -97,6 +100,7 @@ struct add_admin_user {
 };
 
 //Solo Switch Disector y la creacion o destruccion de usuarios recibe DATA
+//Tiene los posibles campos que puede recivir, no deben llenarse todos 
 union data {
     char                            user[USERNAME_SIZE]; // EL usuario a borrar
     enum   disector_data     disector_data_params;
@@ -109,21 +113,32 @@ union data_len {
         uint8_t byte[2];
 };
 
-struct monitor {
+struct protocol {
     char                    token[TOKEN_SIZE];
-    enum  monitor_method    method;
+    enum  protocol_method    method;
     uint16_t                dlen;
     union data              data;
 };
 
-struct monitor_parser {
-    struct monitor *monitor;
-    enum monitor_state state;
+struct protocol_parser {
+    struct protocol *protocol;
+    enum protocol_state state;
     /** cuantos bytes tenemos que leer*/
     uint16_t len;
     /** cuantos bytes ya leimos */
     uint16_t i;
     int separated;
 };
+
+/** inicializa el parser del protocolo */
+void protocol_parser_init(struct protocol_parser *p);
+
+/** consume el buffer read de la coneccion y lo procesa */
+extern enum protocol_state protocol_consume(buffer *b, struct protocol_parser *p);
+
+/** Funcion que devuelve verdadero cuando se finaliza el parseo  */
+bool protocol_is_done(enum protocol_state state);
+
+
 
 #endif
--- src/server/protocol.c
+++ src/server/protocol.c
@@ -8,7 +8,248 @@
 
 #include "protocol.h"
  
+static uint8_t combinedlen[2] = {0};
+static size_t username_len_with_null = 0;
 
+
+/* Si se llego a este estado, ya se finalizo el parseo*/
+bool protocol_is_done(enum protocol_state state){
+    return state >= protocol_done;
+}
+
+/*  Acutaliza los bytes leidos y los que faltan leer para el proximo estado
+    Por ejemplo: cuando termina de leer correctamente la version, setea los bytes leidos en 0 y los que faltan leer en
+    el tamaño del token(con esto el proximo estado puede saber si leyo todo lo que deberia o no)
+*/
+static void remaining_set(struct protocol_parser *p, uint16_t len) {
+    p->i = 0;
+    p->len = len; 
+}
+
+/* Se actualiza el valor i(cantidad leida) por lo que hay que fijarse si le falta leer mas byes,
+ * ya que recordar que se esta leyendo byte a byte
+ */
+static int remaining_is_done(struct protocol_parser *p) {
+    return p->i >= p->len;
+}
+
+/** si el byte recivo no es 0x01, lo manda al estado de unsupported_version, sino pasa al estado token */
+static enum protocol_state version(const uint8_t c, struct protocol_parser *p) {
+    enum protocol_state next;
+    switch (c) {
+        case 0x01:
+            remaining_set(p, TOKEN_SIZE);
+            next = protocol_token;
+            break;
+        default:
+            next = protocol_error_unsupported_version;
+            break;
+    }
+
+    return next;
+}
+
+/** va leyendo y copiando el token, una vez que finalice se pasa el estado de method */
+static enum protocol_state token(const uint8_t c, struct protocol_parser *p) {
+    p->protocol->token[p->i++] = c;
+    if (remaining_is_done(p))
+        return protocol_method;
+    return protocol_token;
+}
+
+/** Como method tiene un solo byte, la funcion method se llamara una unica vez, y se progresara
+ *  como corresponda al metodo correcto, o caera en default y retornara unsoported_method
+ *  Vemos que hay 2 casos muy distintivos, los que requieren de un DATA y los que no
+*/
+static enum protocol_state method(const uint8_t c, struct protocol_parser *p) {
+    enum protocol_state next;
+
+    p->protocol->method = c;
+    switch (p->protocol->method) {
+        case get_historic:
+        case get_concurrent:
+        case get_transfered:
+       
+            p->protocol->method = c;
+            //Le aviso que DLEN debe leer 2 bytes
+            remaining_set(p,DLEN_SIZE);
+            next = protocol_dlen;
+            break;        
+        default:
+            next = protocol_error_unsupported_method;
+            break;
+    }
+
+    return next;
+}
+
+
+/** Como se dijo en el paso anterior, no siempre se llegara a este 
+ *  estado
+ *  Aca se presenta una dificultad, la longitud de DLEN dada en 2 bytes deben ser combinadas
+ *  para esto se asume que vienen en BIGendean y para el resultado de 16 bits se utiliza el endian
+ *  del sistema para evitar errores
+*/
+static enum protocol_state dlen(const uint8_t c, struct protocol_parser *p) {
+    enum protocol_state next;
+
+    combinedlen[p->i++] = c;
+    next = protocol_dlen;
+    //Si se leyeron los 2 bytes
+    if (remaining_is_done(p)) {
+        p->protocol->dlen = ntohs(*(uint16_t*)combinedlen); 
+        switch (p->protocol->method) {
+            //Para el caso de pop3, solo se pide un byte por lo que no se usa remaining_set
+            case pop3disector:
+                next = protocol_data;
+                break;
+            case add_proxyuser:
+                remaining_set(p, p->protocol->dlen);
+                next = protocol_data;
+                break;
+            case delete_proxyuser:
+                remaining_set(p, p->protocol->dlen); 
+                next = protocol_data;
+                break;
+            case add_admin:
+                remaining_set(p, p->protocol->dlen);
+                next = protocol_data;
+                break;
+            case delete_admin:
+                remaining_set(p, p->protocol->dlen);
+                next = protocol_data;
+                break;
+            default:
+                next = protocol_error;
+                break;
+        }
+    }
+
+    return next;
+}
+
+static enum protocol_state data(const uint8_t c, struct protocol_parser *p) {
+    enum protocol_state next;
+
+    switch(p->protocol->method) { 
+        case pop3disector:
+            p->protocol->data.disector_data_params = c;
+            next = protocol_done;
+            break;
+        
+        case add_proxyuser: 
+            // Si el primer caracter es 0 directamente tiro error ya que el usuario no puede ser vacio
+            if (p->i == 0 && c == 0) {
+                next = protocol_error_invalid_data;
+                break;
+            }
+            // user0pass
+            //Si el bit es un caracter alfanumerico, debe ser o nombre o contraseña
+            //Se separa en dos etapas, antes del separador o despues del separador
+            if (IS_ALNUM(c)) {
+                if (p->separated == 0) {
+                    p->protocol->data.add_proxy_user_param.user[p->i++] = c;
+                } else {
+                    p->protocol->data.add_proxy_user_param.pass[p->i - username_len_with_null] = c; //pass[0] = c
+                    p->i++;
+                }
+                next = protocol_data;
+            } else if (c == 0 && p->separated == 0) { 
+                // se econtro el primer separador \0, lo marco y  pongo el null terminated en el username
+                p->protocol->data.add_proxy_user_param.user[p->i++] = c;
+                p->separated = 1;
+                username_len_with_null = p->i;
+                next = protocol_data;
+            } else { 
+                // Si no es alfanumerico y ya hubo un separador, entonces hubo un error por parte del cliente en el data
+                next = protocol_error_invalid_data;
+                break;
+            }
+            //Si se termino de procesar el data y no hubieron errores, se pasa a protocol_done
+            if (remaining_is_done(p)) {
+                p->protocol->data.add_proxy_user_param.pass[p->i] = 0; // null terminated para password
+                next = protocol_done;
+                p->separated = 0;
+                break;
+            }
+
+            break;
+
+        case delete_proxyuser:
+            //Se lee cada caracter alfanumerico, si no lo es, es un error
+            if (IS_ALNUM(c)) {
+                p->protocol->data.user[p->i++] = c;
+                next = protocol_data;
+            } else {
+                next = protocol_error_invalid_data;
+                break;
+            }
+            //Una vez se procesa data completo se le pone el null terminated a user y va a protocol_done
+            if (remaining_is_done(p)) {
+                p->protocol->data.user[p->i] = 0; 
+                next = protocol_done;
+                break;
+            }
+            
+            break;
+
+        case add_admin:
+            // parecico a add_proxy_user
+            if (p->i == 0 && c == 0) {
+                next = protocol_error_invalid_data;
+                break;
+            }
+
+            if (IS_ALNUM(c)) {
+                if (p->separated == 0) {
+                    p->protocol->data.add_admin_user_param.user[p->i++] = c;
+                } else {
+                    p->protocol->data.add_admin_user_param.token[p->i - username_len_with_null] = c;
+                    p->i++;
+                }
+                next = protocol_data;
+            } else if (c == 0 && p->separated == 0) { // primer separador \0 pongo el null terminated en el username
+                p->protocol->data.add_admin_user_param.user[p->i++] = c;
+                p->separated = 1;
+                username_len_with_null = p->i;
+                next = protocol_data;
+            } else { // Si no es alfanumerico ni fue el primer 0 separador entonces no es un dato valido
+                next = protocol_error_invalid_data;
+                break;
+            }
+
+            if (remaining_is_done(p)) {
+                p->protocol->data.add_admin_user_param.token[p->i] = 0; // null terminated para password
+                next = protocol_done;
+                p->separated = 0;
+                break;
+            }
+            
+            break;
+
+        case delete_admin:
+            if (IS_ALNUM(c)) {
+                p->protocol->data.user[p->i++] = c;
+                next = protocol_data;
+            } else {
+                next = protocol_error_invalid_data;
+                break;
+            }
+
+            if (remaining_is_done(p)) {
+                p->protocol->data.user[p->i] = 0; // null terminated para username
+                next = protocol_done;
+                break;
+            }
+            
+            break;
+        default:
+            next = protocol_error_invalid_data;
+            break;
+    }
+
+    return next;
+}
 
 
 
@@ -71,3 +312,4 @@ void protocol_parser_init(struct protocol_parser *p) {
     p->state = protocol_version;
     memset(p->protocol, 0, sizeof(*(p->protocol)));
 }
+
