1757f3d673276e8dbaff2931a3ec7a516fac7c4b
MalenaBanfi <mabanfi@itba.edu.ar>
2022-11-19 21:21:54 -0300

    starting the protocol parsing

--- src/server/protocol.c
+++ src/server/protocol.c
@@ -6,4 +6,22 @@
 #include <stdio.h>
 #include <stdio.h> // printf
 
-#include "protocol.h"
+#include "../include/protocol.h"
+
+// Si se llego a este estado, ya se finalizo el parseo
+bool protocol_is_done(enum protocol_state state){
+    return state >= protocol_done;
+}
+
+// Funcion principal de parseo, es la que consume byte a byte y delega el procesamiento
+extern enum protocol_state protocol_consume(buffer *b, struct protocol_parser *p) {
+    enum protocol_state st = p->state;
+
+    while (buffer_can_read(b)) {
+        const uint8_t c = buffer_read(b);
+        st = protocol_parser_feed(p, c); // cargamos 1 solo byte
+        if (protocol_is_done(st))
+            break;
+    }
+    return st;
+}
