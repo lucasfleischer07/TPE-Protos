950fda785c64930c30ad91037b4c18bb6dc29afd
MalenaBanfi <mabanfi@itba.edu.ar>
2022-11-20 17:10:27 -0300

    Selector error fixed and proxy slightly working

--- src/include/selector.h
+++ src/include/selector.h
@@ -6,9 +6,9 @@
 
 /**
  * selector.c - un muliplexor de entrada salida
- selector permite manejar en un único hilo de ejecución la entrada salida
- * de file descriptors de forma no bloqueante.*
- * Un 
+ *
+ * Un selector permite manejar en un único hilo de ejecución la entrada salida
+ * de file descriptors de forma no bloqueante.
  *
  * Esconde la implementación final (select(2) / poll(2) / epoll(2) / ..)
  *
